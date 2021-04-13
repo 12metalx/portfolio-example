a1db0897b7eae9c1bffca2438b87d48b323e4aa3
12metalx <perezozo79@gmail.com>
2021-04-12 20:28:44 -0500

    Cambio en paleta de colores y seccio about

---
+++ ts/main.ts
@@ -0,0 +1,9 @@
+const hire = document.getElementById("hire");
+const proj = document.getElementById("proj");
+
+const redirect = () => {
+    window.open("https://github.com/12metalx",'_blank');
+    
+}
+hire.addEventListener("click",redirect);
+proj.addEventListener("click",redirect);
